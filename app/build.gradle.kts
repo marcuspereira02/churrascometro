7e148864837d3d3896ea05725edfdc1068823334
Marcus Vinícius de Sá Pereira <makitasjc@outlook.com>
2025-01-04 14:48:48 -0300

    layout tela inicial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.marcuspereira.churrascometro"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.marcuspereira.churrascometro"
