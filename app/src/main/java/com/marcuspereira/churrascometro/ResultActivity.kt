7b8c78b2d0d10aaf49fae5b2b03433b2ea71c3ed
Marcus Vinícius de Sá Pereira <makitasjc@outlook.com>
2025-02-07 16:04:36 -0300

    enxugando código e melhorando interface

--- app/src/main/java/com/marcuspereira/churrascometro/ResultActivity.kt
+++ app/src/main/java/com/marcuspereira/churrascometro/ResultActivity.kt
@@ -1,6 +1,5 @@
 package com.marcuspereira.churrascometro
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
@@ -8,10 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
-const val KEY_RESULT_ACTIVITY = "ResultActivity.Key"
+import com.marcuspereira.churrascometro.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,27 +22,29 @@ class ResultActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val resultadoQtdCarne : Float = intent.getFloatExtra(KEY_RESULT_ACTIVITY, 0f)
-        val resultadoQtdCerveja : Float = intent.getFloatExtra("QTDCERVEJA", 0f)
-        val resultadoQtdRefri : Float = intent.getFloatExtra("QTDREFRIGERANTE", 0f)
-        val btnNovoCalculo = findViewById<Button>(R.id.btn_NovoCalculo)
 
-        val qtdCarneFormatado = String.format("%.2f", resultadoQtdCarne)
-        val qtdCervejaFormatado = String.format("%.2f", resultadoQtdCerveja)
-        val qtdRefriFormatado = String.format("%.2f", resultadoQtdRefri)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val tvQtdCarne = findViewById<TextView>(R.id.tv_QtdCarne)
-        val tvQtdCerveja = findViewById<TextView>(R.id.tv_QtdCerveja)
-        val tvQtdRefri = findViewById<TextView>(R.id.tv_QtdRefri)
+        val resultMeatAmount: Float = intent.getFloatExtra("amountMeat", 0.0f)
+        val resultBeerAmount: Float = intent.getFloatExtra("amountBeer", 0.0f)
+        val resultRefrigerantAmount: Float = intent.getFloatExtra("amountRefrigerant", 0.0f)
 
-        tvQtdCarne.text = "Quantidade de carne (KG): $qtdCarneFormatado"
-        tvQtdCerveja.text = "Quantidade de cerveja (Litros): $qtdCervejaFormatado"
-        tvQtdRefri.text = "Quantidade de refrigerante (Litros): $qtdRefriFormatado"
+        val amountMeat = numFormat(resultMeatAmount)
+        val amountBeer = numFormat(resultBeerAmount)
+        val amountRefrigerant = numFormat(resultRefrigerantAmount)
 
-        btnNovoCalculo.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra(KEY_MAIN_ACTIVITY, "")
-            startActivity(intent)
+        binding.tvAmountMeat.text = "Quantidade de carne (KG): $amountMeat"
+        binding.tvAmountBeer.text = "Quantidade de cerveja (Litros): $amountBeer"
+        binding.tvAmountRefrigerant.text = "Quantidade de refrigerante (Litros): $amountRefrigerant"
+
+        binding.btnRefresh.setOnClickListener {
+            finish()
         }
+    }
+
+    private fun numFormat(num: Float): String {
+        val formatNum = String.format("%.2f", num)
+        return formatNum
     }
 }
